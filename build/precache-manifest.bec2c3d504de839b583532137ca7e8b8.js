57c5bdf016415836a0bd1c64681224b8ab0d167a
Moin-A <Quintype@101992>
2019-10-22 10:59:41 +0530

    changes

---
+++ build/precache-manifest.bec2c3d504de839b583532137ca7e8b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c3bb1bcec5be295b808f9fec8b7f50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e282f559f8d63106b0",
+    "url": "/static/css/main.21320ed7.chunk.css"
+  },
+  {
+    "revision": "7b9d9bc9f2f66c733ef1",
+    "url": "/static/js/2.b716873a.chunk.js"
+  },
+  {
+    "revision": "64e282f559f8d63106b0",
+    "url": "/static/js/main.72cdad76.chunk.js"
+  },
+  {
+    "revision": "41984644ab59b9ef8d8a",
+    "url": "/static/js/runtime-main.038571fa.js"
+  }
+]);
